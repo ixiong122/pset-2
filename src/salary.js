25f445d5a5f11a806be9c094cc8240d19928a69c
ixiong122 <55449385+ixiong122@users.noreply.github.com>
2019-10-25 11:42:14 -0400

    Update salary.js

--- src/salary.js
+++ src/salary.js
@@ -7,3 +7,8 @@ const socialSecurityTax=.062;
 const medicareTax=.0145;
 let biweeklySalary=annualSalary/24;
 let preTaxSalary=biweeklySalary*(1-preTax);
+const addedTaxes=federalIncomeTax+stateIncomeTax+socialSecurityTax+medicareTax;
+const addedTaxes2=1-addedTaxes;
+let finalSalary=preTaxSalary*addedTaxes2;
+const formatted=finalSalary.toLocaleString("en", {minimumFractionDigits:2, maximumFractionDigits: 2});
+console.log("Your take-home pay each check will be $"+formatted+".");
